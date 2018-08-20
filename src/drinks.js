1b9132dfac788e9d4cf843d02b2b5776f322d5d5
Alex Rankin <alex.c.rankin@gmail.com>
2018-08-20 14:17:49 -0700

    drink data, wiring up components

---
+++ src/drinks.js
@@ -0,0 +1,48 @@
+const drinks = [
+  {
+    name: 'Ramos Gin Fizz',
+    baseSpirit: 'gin',
+    year: 1888,
+    era:'pre-Prohibition',
+    containsEgg: true
+  },
+  {
+    name: 'Mai Tai',
+    baseSpirit: 'rum',
+    year: 1944,
+    era: 'Tiki',
+    containsEgg: false
+
+  },
+  {
+    name: 'Sazerac',
+    baseSpirit: 'whiskey',
+    year: 1838,
+    era: 'pre-Prohibition',
+    containsEgg: false
+
+  },
+  {
+    name: 'Sazerac',
+    baseSpirit: 'gin',
+    year: 1888,
+    era: 'pre-Prohibition',
+    containsEgg: false
+  },
+  {
+    name: 'Pisco Sour',
+    baseSpirit: 'pisco',
+    year: 1903,
+    era: 'pre-Prohibition',
+    containsEgg: true
+  },
+  {
+    name: 'Zombie',
+    baseSpirit: 'rum',
+    year: 1934,
+    era: 'Tiki',
+    containsEgg: false
+  }
+];
+
+export default drinks;
