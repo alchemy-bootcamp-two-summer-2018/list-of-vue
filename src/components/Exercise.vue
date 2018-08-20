8822a90281ecfa4ae1e4ce10dc1e09da427f7fca
Jack <ajackofsometrades@gmail.com>
2018-08-20 16:38:11 -0700

    adding conditional rendering

--- src/components/Exercise.vue
+++ src/components/Exercise.vue
@@ -4,9 +4,17 @@
     <p>Sets: {{ exercise.sets }}</p>
     <p>Reps: {{ exercise.reps }}</p>
     <p>Notes: {{ exercise.notes }} </p>
+    <p 
+      v-if="exercise.name.toLowerCase() !== 'squat'" style="color: red;">
+      don't skip leg day
+    </p>
+    <p v-else
+      style="color: blue;">
+      do you even lift?
+    </p>
     <button
-        v-on:click="onRemove(exercise)">
-        Remove this exercise
+      v-on:click="onRemove(exercise)">
+      Remove this exercise
     </button>
   </li>
 </template>
