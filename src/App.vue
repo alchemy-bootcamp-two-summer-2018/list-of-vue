61023671a0c9a635035b3a1ef2bd3429b7211cc7
Jack <ajackofsometrades@gmail.com>
2018-08-20 16:07:31 -0700

    sending handleRemove down...

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
   <div id="app">
     <h1>Workout Tracker</h1>
     <img alt="workout logo" src="./assets/logo.png">
-    <AddExerciseForm v-bind:onAdd="handleAdd" />
-    <ExerciseList v-bind:exercises="exercises" />
+    <AddExerciseForm 
+      v-bind:onAdd="handleAdd" 
+    />
+    <ExerciseList 
+      v-bind:exercises="exercises" 
+      v-bind:onRemove="handleRemove"
+    />
   </div>
 </template>
 
@@ -24,6 +29,12 @@ export default {
   methods: {
     handleAdd(exercise) {
       this.exercises.push(exercise);
+    },
+    handleRemove(exercise) {
+      const index = this.exercises.indexOf(exercise);
+      if(index !== -1) {
+        this.exercises.splice(index,1);
+      }
     }
 
   }
