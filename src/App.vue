760fb3dee6a40a034d1b5c77078ea82ff32faf3c
Sarah Flynn <sarah.flynn@icloud.com>
2018-08-20 14:08:28 -0700

    posting test message to localhost

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <CuteAnimalForm msg="This is a test"/>
+  </div>
+</template>
+
+<script>
+import CuteAnimalForm from './components/CuteAnimalForm.vue';
+// import cuteAnimals from '.cute-animals.js';
+
+export default {
+  name: 'app',
+  components: {
+    CuteAnimalForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/cute-animals.js
@@ -0,0 +1,19 @@
+const cuteAnimals = [
+    {
+        name: 'Floof',
+        type: 'Kitten',
+        img: 'https://i.ytimg.com/vi/J0EUxEkm4kc/0.jpg'
+    },
+    {
+        name: 'Spike',
+        type: 'Hedgehog',
+        img: 'https://images.pexels.com/photos/50577/hedgehog-animal-baby-cute-50577.jpeg?auto=compress&cs=tinysrgb&h=350'
+    },   
+    {
+        name: 'Yertle',
+        type: 'Turtle',
+        img: 'https://i.ytimg.com/vi/2McN9qOw8t4/hqdefault.jpg'
+    },
+];
+
+export default cuteAnimals;
