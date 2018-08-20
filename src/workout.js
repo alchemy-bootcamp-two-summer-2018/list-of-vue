062411e9cc399e90b353a0a138637cee832fb09a
Jack <ajackofsometrades@gmail.com>
2018-08-20 14:14:42 -0700

    starting workout data

---
+++ src/workout.js
@@ -0,0 +1,22 @@
+const workout = [
+  {
+    name: 'bench',
+    sets: 4,
+    reps: 8,
+    notes: 'Was easy'
+  },
+  {
+    name: 'squat',
+    sets: 5,
+    reps: 5,
+    notes: 'Was hard'
+  },
+  {
+    name: 'deadlift',
+    sets: 3,
+    reps: 12,
+    notes: 'Wasn\'t too bad'
+  },
+];
+
+export default workout;
