1e64326a4cdca27b2643ba89ba2eca96cd542ec2
Dyanna Tolman <dyannast@gmail.com>
2018-08-21 02:08:37 -0700

    app and js files

---
+++ src/components/AddDish.vue
@@ -0,0 +1,46 @@
+<template>
+    <form v-on:submit.prevent="handleSubmit">
+        <p>
+            <label>
+                Name: <input v-model="name">
+            </label>
+        </p>
+        <p>
+            <label>
+                Ethnic-Origin: <input v-model="ethnicOrigin">
+            </label>
+        </p>
+        <p>
+        <button>Add Dish</button>
+        </p>
+    </form>
+</template>
+
+<script>
+export default {
+  props: {
+    onAdd: Function
+  },
+  data() {
+    return {
+      name:'',
+      ethnicOrigin: ''
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const newDish = {
+        name: this.name,
+        ethnicOrigin: this.ethnicOrigin
+      };
+      this.onAdd(newDish);
+
+      this.name = '';
+      this.ethnicOrigin = '';
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/dishes.js
@@ -0,0 +1,28 @@
+const dishes = [
+    {
+      name: 'Hainanese Chicken Rice',
+      image: '/images/ChickenRice.jpg',
+      ethnicOrigin: 'Chinese',
+      spiceLevel: 'mild'
+    },
+    {
+      name: 'Mee Soto',
+      image: '/images/MeeSoto.jpg',
+      ethnicOrigin: 'Malay',
+      spiceLevel: 'mild'
+    },
+    {
+      name: 'Char Kway Teow',
+      image: '/images/CharKway.jpg',
+      ethnicOrigin: 'Chinese',
+      spiceLevel: 'medium'
+    },
+    {
+      name: 'Mee Goreng Mamak',
+      image: '/images/MeeGoreng.jpg',
+      ethnicOrigin: 'Indian',
+      spiceLevel: 'spicy'
+    }
+  ];
+    
+  export default dishes;
