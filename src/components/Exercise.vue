81d54bcd47c3bef6863ed1f08abbbd6bb42e6f3a
Jack <ajackofsometrades@gmail.com>
2018-08-22 07:44:39 -0700

    removed lengthy comment

--- src/components/Exercise.vue
+++ src/components/Exercise.vue
@@ -4,14 +4,6 @@
     <p>Sets: {{ exercise.sets }}</p>
     <p>Reps: {{ exercise.reps }}</p>
     <p>Notes: {{ exercise.notes }} </p>
-    <!-- <p 
-      v-if="exercise.name.toLowerCase() !== 'squat'" style="color: red;">
-      don't skip leg day
-    </p>
-    <p v-else
-      style="color: blue;">
-      do you even lift?
-    </p> -->
     <button
       v-on:click="onRemove(exercise)">
       Remove this exercise
