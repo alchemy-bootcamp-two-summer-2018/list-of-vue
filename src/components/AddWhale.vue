73d132d9244f6452cf127a084f138ff9175b8e54
Claire Flanagan <claireflanagan@gmail.com>
2018-08-26 14:28:17 -0700

    whales!

---
+++ src/components/AddWhale.vue
@@ -0,0 +1,56 @@
+<template>
+    <form v-on:submit.prevent="handleSubmit">
+        <h2>Add a whale!</h2>
+        <p>
+            <label>
+                Species: <input v-model="species" placeholder="Name of Species">
+            </label>
+        </p>
+        <p>
+            <label>
+                Weight: <input v-model="weight" placeholder="Average Weight">
+            </label>
+        </p>
+        <p>
+            <label>
+                URL: <input v-model="url" placeholder="Insert URL Here">
+            </label>
+        </p>
+        <p>
+            <button>Submit</button>
+        </p>
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        onAdd: Function
+    },
+    data() {
+        return {
+            species: '',
+            weight: '',
+            url: ''
+        };
+    },
+    methods: {
+        handleSubmit() {
+            const newWhale = {
+                species: this.species,
+                weight: this.weight,
+                url: this.url
+            };
+        this.onAdd(newWhale);
+
+        this.species = '';
+        this.weight = '';
+        this.url = '';
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/whales.js
@@ -0,0 +1,19 @@
+const whales = [
+    {
+        species: 'Humpback Whale',
+        weight: '25-30 metric tons',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Humpback_whales_in_singing_position.jpg'
+    },
+    {
+        species: 'Killer Whale',
+        weight: '8-10 metric tons',
+        url: 'https://www.publicdomainpictures.net/pictures/220000/velka/killer-whales.jpg'
+    },
+    {
+        species: 'Blue Whale',
+        weight: '100-200 metric tons',
+        url: 'https://www.goodfreephotos.com/cache/animals/mammals/blue-whale-balaenoptera-musculus_800.jpg'
+    }
+];
+
+export default whales;
