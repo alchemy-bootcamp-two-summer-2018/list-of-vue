1db4cfc18781ecf8b5743a43c7c458d1ea7fc82d
Dyanna Tolman <dyannast@gmail.com>
2018-08-26 21:39:41 -0700

    revised

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class='content'>
+    <img alt="hawker center" src="./assets/hawker.jpg">
+    <h1>Welcome to the streets of Singapore!</h1>
+    <DishList v-bind:dishes="dishes" v-bind:onRemove="handleRemove"/>
+    <p class='question'>Do you have a favorite Singapore dish that you would like to add to this list? You can below!</p>
+    <AddDish v-bind:onAdd="handleAdd"/>
+  </section>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DishList from './components/DishList.vue';
+import AddDish from './components/AddDish.vue';
+import dishes from './dishes';
 export default {
   name: 'app',
+  data() {
+    return { dishes };
+  },
   components: {
-    HelloWorld
+    AddDish,
+    DishList
+  },
+  methods: {
+    handleAdd(dish) {
+      this.dishes.push(dish);
+    },
+    handleRemove(dish) {
+      const index = this.dishes.indexOf(dish);
+      if(index !== -1) {
+        this.dishes.splice(index, 1);
+      }
+    }
   }
-}
+};
 </script>
-
-<style>
+ <style>
+h1 {
+  font-family: 'Josefin Sans', sans-serif;
+  color: red;
+  font-size: 30px;
+}
+.content {
+  text-align:center;
+}
+.question {
+  color:red;
+  font-weight:bold;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
